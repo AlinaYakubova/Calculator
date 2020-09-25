b0bae4a72c76fcd4174b4d6dbbfe2c9fe07264d1
Алина Якубова <alinatakubova19@gmail.com>
2020-09-25 20:58:56 +0300

    Update calc.py
    Добавлено разбиение на функции

--- calc.py
+++ calc.py
@@ -1,5 +1,3 @@
-
-
 def greetings():
 	greetings_str = "Hey! Welcome to calculator"
 	print(greetings_str)
@@ -19,7 +17,7 @@ def div_two_numbs(a, b):
 	return a / b
 
 def check_for_exit(user_input):
-	if str(user_input) == "exit"
+	if str(user_input) == "exit":
 		return True
 	return False
 
@@ -30,63 +28,66 @@ def convert_to_proper_type(user_input):
 		user_input = int(user_input)
 	return user_input
 
-def run()
+def handle_user_input_numbers(user_input):
 	while True:
-		handle_user_input()
-		calculate()
+		if check_for_exit(user_input):
+			return None
+			break
+		else:
+			try:
+				user_number = convert_to_proper_type(user_input)
+				break
+			except ValueError:
+				print("Please enter a number")
+				continue
+	return user_number
 
-def hanlde_user_input()
-	[first_numb, second_numb, operation]
+def handle_user_input_operation():
+	while True:
+		operation = input("Enter an operator: ")
+		if operation  not in ["+", "-", "*", "/"]:
+			print("Please enter valid operation")
+			continue
+		else:
+			break
+	return operation
 
-def calculate(a, b, operation)
+def calculate(first_numb, second_numb, operation):
+	if operation == "+":
+		result = sum_two_numbs(first_numb, second_numb)
+	elif operation == "*":
+		result = mul_two_numbs(first_numb, second_numb)
+	elif operation == "-":
+		result = sub_two_numbs(first_numb, second_numb)
+	elif operation == "/":
+		try:
+			result = div_two_numbs(first_numb, second_numb)
+		except ZeroDivisionError:
+			result = "You cannot divide by zero!"
+	else:
+		result = "Oops! Please enter valid operation."
+	print(result)
+	# return result
+
+
+
+def run():
+	while True:
+		user_input_1 = input("Enter first number: ")
+		first_numb = handle_user_input_numbers(user_input_1)
+		if first_numb is None:
+			break
+		user_input_2 = input("Enter second number: ")
+		second_numb = handle_user_input_numbers(user_input_2)
+		if second_numb is None:
+			break
+		operation = handle_user_input_operation()
+		calculate(first_numb, second_numb, operation)
+
 
 def main():
 	greetings()
-	while True:
-		while True:
-			first_numb = input("Enter first number: ")
-			if exit_and_warning_func(first_numb):
-				return 0
-			else:
-				try:
-					first_numb = convert_to_proper_type(first_numb)
-					break
-				except ValueError:
-					print("Please enter a number")
-					continue
-
-		while True:
-			second_numb = input("Enter second number: ")
-			error_code = exit_and_warning_func(second_numb)
-			if error_code == -1:
-				return 0
-			elif error_code == 1:
-				try:
-					second_numb = convert_to_proper_type(second_numb)
-					break
-				except ValueError:
-					print("Please enter a number")
-					continue
-
-		while True:
-			operation = input("Enter an operator: ")
-			if operation  not in ["+", "-", "*", "/"]:
-				print("Please enter valid operation")
-				continue
-			else:
-				break
-
-		if operation == "+":
-			result = sum_two_numbs(first_numb, second_numb)
-		elif operation == "*":
-			result = mul_two_numbs(first_numb, second_numb)
-		elif operation == "-":
-			result = sub_two_numbs(first_numb, second_numb)
-		elif operation == "/":
-			result = div_two_numbs(first_numb, second_numb)
-		else:
-			result = "Oops! Please enter valid operation."
-		print(result)
+	run()
 	return 0
 
 if __name__ == "__main__":
